e1a9d00a3d8154c20a85c56271e496e1f0d0fee6
Luigi_linux <gigio996@yahoo.it>
2025-01-28 12:16:17 +0100

    main embrionale

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.postgres_extractor.postgres_ddl_extractor import PostgresExtractor as pg
+from src.mysql_extractor.mysql_ddl_extractor import MySQlExtractor as ms
 import json
 import sys
 
@@ -17,8 +18,18 @@ if __name__ == '__main__':
     HOST = config["host"]             # Usa 'localhost' per connetterti al container
     PORT = config["port"]                  # Porta esposta dal container PostgreSQL
 
+    #estrazione pgres
     pgres = pg(DATABASE, USER, PASSWORD, HOST, PORT)
     pgres.create_connection()
     ddls = pgres.estrai_ddl_from_query()
     pgres.close_connection()
     pg.save_ddl(saving_ddl_path, ddls)
+    pg.save_ddl_single_file(saving_ddl_path, ddls)
+
+    # #estrazione mysql
+    # mysql = ms(USER, PASSWORD, HOST, PORT)
+    # mysql.create_connection()
+    # ddls = mysql.extract_ddl(DATABASE)
+    # mysql.close_connection()
+    # #ms.save_ddl(saving_ddl_path, ddls)
+    # ms.save_ddl_single_file(saving_ddl_path, ddls)
